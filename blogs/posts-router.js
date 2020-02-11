1d346a4d7b4da978dce677d0ef3a9bd08154ad75
Zachary Mayfield <zakmayfield@gmail.com>
2020-02-11 15:37:43 -0500

    all get requests working, not all verified

--- blogs/posts-router.js
+++ blogs/posts-router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Posts = require('../data/db.js')
 const router = express.Router()
 
+
 router.get('/', (req, res) => {
   Posts.find()
     .then(posts => {
@@ -14,5 +15,41 @@ router.get('/', (req, res) => {
       })
     })
 })
+// verified
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params
+  //id that does not exist gives back []
+  // post = [] ? 404 : post *doesn't work*
+
+  Posts.findById(id)
+    .then(post => {
+      res.status(200).json(post)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        message: `Couldn't retrieve that for you`
+      })
+    })
+})
+//NOT verified
+
+router.get('/:id/comments', (req, res) => {
+  const {id} = req.params
+
+  Posts.findPostComments(id)
+    .then(comments => {
+      res.status(200).json(comments)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        message: `Couldn't retrieve that for you`
+      })
+    })
+})
+//NOT verified
+
 
 module.exports = router;
